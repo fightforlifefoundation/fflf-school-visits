08499c512ec7e549ae7c257169d0d1d7a6a25b1b
Andrew Kolbus <akolbus@salesforce.com>
2018-10-18 14:24:48 -0400

    create table for visits

--- fflf-school-visits.php
+++ fflf-school-visits.php
@@ -3,6 +3,44 @@
 Plugin Name: FFLF School Visits
 */
 
+global $fflf_sv_db_version;
+$fflf_sv_db_version='0.1';
+
+function fflf_sv_install() {
+    global $wpdb;
+    global $fflf_sv_db_version;
+
+    $table_name= $wpdb->prefix . "fflfSchoolVisits";
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        eventId INT NOT NULL AUTO_INCREMENT,
+        schoolName VARCHAR(1024) NOT NULL,
+        className VARCHAR(1024) NOT NULL,
+        grade VARCHAR(10) NOT NULL,
+        startDate DATETIME NOT NULL,
+        endDate DATETIME NOT NULL,
+        schoolAddress VARCHAR(1024) NOT NULL,
+        schoolCity VARCHAR(1024) NOT NULL,
+        schoolState VARCHAR(2) NOT NULL,
+        schoolZip VARCHAR(20) NOT NULL,
+        claimed TINYINT(1) NOT NULL DEFAULT 0,
+        sponsorCompany VARCHAR(1024) NULL,
+        sponsorFirstName VARCHAR(1024) NULL,
+        sponsorLastName VARCHAR(1024) NULL,
+        sponsorEmail VARCHAR(1024) NULL,
+        sponsorPhone VARCHAR(50) NULL,  
+        sponsorClaimDate DATETIME NULL,      
+        PRIMARY KEY  (eventId)
+    ) $charset_collate;";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+
+    add_option('fflf_sv_db_version', $fflf_sv_db_version );
+}
+
 //GET wp-json/fflf-school-visits/visits
 function get_unclaimed_visits(){
     $return_arr = array();
@@ -49,6 +87,15 @@ function get_unclaimed_visits(){
     return $return_arr;
     //return json_encode($return_arr);
 }
+
+function fflf_sv_update_db_check(){
+    global $fflf_sv_db_version;
+    if ( get_site_option( 'fflf_sv_db_version' ) != $fflf_sv_db_version ) {
+        fflf_sv_install();
+    }
+}
+
+add_action( 'plugins_loaded', 'fflf_sv_update_db_check' );
 
 add_action( 'rest_api_init', function () {
     register_rest_route( 'fflf-school-visits', '/visits', array(
