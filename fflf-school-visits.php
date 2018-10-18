57320a6bababa54805f69aca1f39e7ad8cf297e0
Andrew Kolbus <akolbus@salesforce.com>
2018-10-18 13:18:31 -0400

    refactor dummy data

--- fflf-school-visits.php
+++ fflf-school-visits.php
@@ -6,18 +6,19 @@ Plugin Name: FFLF School Visits
 //GET wp-json/fflf-school-visits/visits
 function get_unclaimed_visits(){
     $return_arr = array();
-    $row_array['eventId'] = 1;
-    $row_array['schoolName'] = 'dummy school 1';
-    $row_array['className'] = 'dummy class 1';
-    $row_array['grade'] = '5-6';
-    $row_array['startDate'] = '2018-10-27 10:00';
-    $row_array['endDate'] = '2018-10-27 15:00';
-    $row_array['schoolAddress'] = '123 Main Street';
-    $row_array['schoolCity'] = 'Indianapolis';
-    $row_array['schoolState'] = 'IN';
-    $row_array['schoolZip'] = '46203'; 
-
-    array_push($return_arr, $row_array);
+    
+    array_push($return_arr, array(
+        'eventId' => 1,
+        'schoolName' => 'dummy school 1',
+        'className' => 'dummy class 1',
+        'grade' => '5-6',
+        'startDate' => '2018-10-27 10:00',
+        'endDate' => '2018-10-27 15:00',
+        'schoolAddress' => '123 Main Street',
+        'schoolCity' => 'Indianapolis',
+        'schoolState' => 'IN',
+        'schoolZip' => '46203' 
+    ));
 
     return $return_arr;
     //return json_encode($return_arr);
